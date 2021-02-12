f9f27a1855f585fef5d445319d2865c24f2a4736
jao <jao@gnu.org>
2021-02-12 21:00:09 +0000

    literate style

--- consult-notmuch.el
+++ consult-notmuch.el
@@ -1,7 +1,5 @@
 ;;; consult-notmuch.el --- Notmuch search using consult  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021  jao
-
 ;; Author: Jose A Ortega Ruiz <jao@gnu.org>
 ;; Maintainer: Jose A Ortega Ruiz
 ;; Keywords: mail
@@ -9,6 +7,9 @@
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "26.1") (consult "0.5") (notmuch "0.21"))
 ;; Homepage: https://codeberg.org/jao/consult-notmuch
+
+
+;; Copyright (C) 2021  Jose A Ortega Ruiz
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -29,6 +30,10 @@
 ;; emails and present results either as single emails
 ;; (`consult-notmuch') or full trees (`consult-notmuch-tree').
 
+;; This elisp file is automatically generated from its literate
+;; counterpart at
+;; https://codeberg.org/jao/consult-notmuch/src/branch/main/readme.org
+
 ;;; Code:
 
 (require 'consult)
@@ -38,81 +43,24 @@
   "Options for consult-notmuch."
   :group 'Notmuch)
 
-(defcustom consult-notmuch-command "notmuch search *ARG*"
-  "Command to perform notmuch search."
-  :type 'string
-  :group 'consult-notmuch)
-
 (defface consult-notmuch-date-face
   '((t :inherit notmuch-search-date :background nil))
-  "Default face used in tree mode face for matching messages"
-  :group 'consult-notmuch)
+  "Face used in matching messages for the date field.")
 
 (defface consult-notmuch-count-face
   '((t :inherit notmuch-search-count :background nil))
-  "Default face used in tree mode face for matching messages"
-  :group 'consult-notmuch)
+  "Face used in matching messages for the mail count field.")
 
-(defface consult-notmuch-people-face
+(defface consult-notmuch-authors-face
   '((t :inherit notmuch-search-matching-authors :background nil))
-  "Default face used in tree mode face for matching messages"
-  :group 'consult-notmuch)
+  "Face used in matching messages for the authors field.")
 
 (defface consult-notmuch-subject-face
   '((t :inherit notmuch-search-subject :background nil))
-  "Default face used in tree mode face for matching messages"
-  :group 'consult-notmuch)
-
-(defvar consult-notmuch-history nil
-  "History for `consult-notmuch'.")
-
-(defvar consult-notmuch--buffer-name "*consult-notmuch-show*"
-  "Name of preview and result buffers.")
-
-(defun consult-notmuch--close-preview ()
-  "Name says it all (and checkdoc is a bit silly)."
-  (when (get-buffer consult-notmuch--buffer-name)
-    (kill-buffer consult-notmuch--buffer-name)))
-
-(defun consult-notmuch--tree (thread)
-  "Open resulting THREAD in ‘notmuch-tree’ view with INITIAL-INPUT."
-  (consult-notmuch--close-preview)
-  (let ((thread-id (car (split-string thread "\\ +"))))
-    (notmuch-tree thread-id initial-input nil)))
-
-(defun consult-notmuch--show (thread)
-  "Open resulting THREAD in ‘notmuch-show’ view."
-  (consult-notmuch--close-preview)
-  (let ((title (concat consult-notmuch--buffer-name (substring thread 24)))
-        (thread-id (car (split-string thread "\\ +"))))
-    (notmuch-show thread-id nil nil nil title)))
-
-(defun consult-notmuch--preview (thread _restore)
-  "Open resulting THREAD in ‘notmuch-show’ view, in a preview buffer."
-  (consult-notmuch--close-preview)
-  (let ((thread-id (car (split-string thread "\\ +"))))
-    (notmuch-show thread-id nil nil nil consult-notmuch--buffer-name)))
-
-(defun consult-notmuch--transformer (str)
-  "Transform STR to notmuch display style."
-  (when (string-match "thread:" str)
-    (let* ((thread-id (car (split-string str "\\ +")))
-           (date (substring str 24 37))
-           (mid (substring str 24))
-           (mat0 (string-match "[[]" mid))
-           (mat1 (string-match "[]]" mid))
-           (mat (substring mid mat0 (1+ mat1)))
-           (people (truncate-string-to-width
-                    (string-trim (nth 1 (split-string mid "[];]"))) 20))
-           (subject (truncate-string-to-width
-                     (string-trim (nth 1 (split-string mid "[;]")))
-                     (- (frame-width) 32))))
-      (format "%s %s\t%10s\t%20s\t%s"
-              (propertize thread-id 'invisible t)
-              (propertize date 'face 'consult-notmuch-date-face)
-              (propertize mat 'face 'consult-notmuch-count-face)
-              (propertize people 'face 'consult-notmuch-people-face)
-              (propertize subject 'face 'consult-notmuch-subject-face)))))
+  "Face used in matching messages for the subject field.")
+(defcustom consult-notmuch-command "notmuch search *ARG*"
+  "Command to perform notmuch search."
+  :type 'string)
 
 (defun consult-notmuch--search (&optional initial)
   "Perform an asynchronous notmuch search via `consult--read'.
@@ -125,6 +73,61 @@ If given, use INITIAL as the starting point of the query."
                  :history 'consult-notmuch-history
                  :state #'consult-notmuch--preview
                  :category 'notmuch-result))
+
+(defvar consult-notmuch-history nil
+  "History for `consult-notmuch'.")
+
+(defun consult-notmuch--transformer (str)
+  "Transform STR to notmuch display style."
+  (when (string-match "thread:" str)
+    (let* ((thread-id (car (split-string str "\\ +")))
+           (date (substring str 24 37))
+           (mid (substring str 24))
+           (c0 (string-match "[[]" mid))
+           (c1 (string-match "[]]" mid))
+           (count (substring mid c0 (1+ c1)))
+           (authors (truncate-string-to-width
+                     (string-trim (nth 1 (split-string mid "[];]"))) 20))
+           (subject (truncate-string-to-width
+                     (string-trim (nth 1 (split-string mid "[;]")))
+                     (- (frame-width) 32))))
+      (format "%s %s\t%10s\t%20s\t%s"
+              (propertize thread-id 'invisible t)
+              (propertize date 'face 'consult-notmuch-date-face)
+              (propertize count 'face 'consult-notmuch-count-face)
+              (propertize authors 'face 'consult-notmuch-authors-face)
+              (propertize subject 'face 'consult-notmuch-subject-face)))))
+
+(defvar consult-notmuch--buffer-name "*consult-notmuch-show*"
+  "Name of preview and result buffers.")
+
+(defun consult-notmuch--close-preview ()
+  "Name says it all (and checkdoc is a bit silly, insisting on this)."
+  (when (get-buffer consult-notmuch--buffer-name)
+    (kill-buffer consult-notmuch--buffer-name)))
+
+
+(defun consult-notmuch--preview (thread _restore)
+  "Open resulting THREAD in ‘notmuch-show’ view, in a preview buffer."
+  (consult-notmuch--close-preview)
+  (let ((thread-id (car (split-string thread "\\ +"))))
+    (notmuch-show thread-id nil nil nil consult-notmuch--buffer-name)))
+
+
+(defun consult-notmuch--show (thread)
+  "Open resulting THREAD in ‘notmuch-show’ view."
+  (consult-notmuch--close-preview)
+  (let ((title (concat consult-notmuch--buffer-name (substring thread 24)))
+        (thread-id (car (split-string thread "\\ +"))))
+    (notmuch-show thread-id nil nil nil title)))
+
+
+(defun consult-notmuch--tree (thread)
+  "Open resulting THREAD in ‘notmuch-tree’."
+  (consult-notmuch--close-preview)
+  (let ((thread-id (car (split-string thread "\\ +"))))
+    (notmuch-tree thread-id nil nil)))
+
 
 ;;;###autoload
 (defun consult-notmuch (&optional initial)
